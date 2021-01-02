205e7182614441b247d883f4f5bba473f2c3da88
Niwako Sugimura <niwako@gmail.com>
2021-01-01 16:12:11 -0800

    Integrate with Firebase and install prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "firebase/database";
+import {
+  FirebaseAppProvider,
+  useFirestore,
+  useFirestoreDocData,
+} from "reactfire";
+// eslint-disable-next-line
+import { SuspenseWithPerf } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLJ57nWDJKrYt-ACW2dhLzUbFSxCdwDv4",
+  authDomain: "hagemashi-prod.firebaseapp.com",
+  projectId: "hagemashi-prod",
+  storageBucket: "hagemashi-prod.appspot.com",
+  messagingSenderId: "297349595147",
+  appId: "1:297349595147:web:5ec28f2f82cb009820c70e",
+  measurementId: "G-B0DT0W1LW7",
+};
+
+function Burrito() {
+  // easily access the Firestore library
+  const burritoRef = useFirestore().collection("tryreactfire").doc("burrito");
+
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData(burritoRef);
+
+  // easily check the loading status
+  if (status === "loading") {
+    return <p>Fetching burrito flavor...</p>;
+  }
+
+  return <p>The burrito is {data.yummy ? "good" : "bad"}!</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <h1>🌯</h1>
+      <Burrito />
+    </FirebaseAppProvider>
   );
 }
 
